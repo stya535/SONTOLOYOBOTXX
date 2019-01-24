e4e8972567db2353a383d5abb8f976c473afd8ba
Genta <35428298+stya535@users.noreply.github.com>
2019-01-24 17:28:17 +0300

    Add files via upload

--- TEGUH/__init__.py
+++ TEGUH/__init__.py
@@ -1,13 +1,4 @@
-from .client import LineClient
-from .channel import LineChannel
-from .poll import LinePoll
-from akad.ttypes import OpType
-
-__copyright__       = 'Copyright 2017 by Fadhiil Rachman'
-__version__         = '2.0.2'
-__license__         = 'BSD-3-Clause'
-__author__          = 'Fadhiil Rachman'
-__author_email__    = 'fadhiilrachman@gmail.com'
-__url__             = 'http://github.com/fadhiilrachman/line-py'
-
-__all__ = ['LineClient', 'LineChannel', 'LinePoll', 'OpType']
+#LICENCE :   http://www.apache.org/licenses/LICENSE-2.0
+#CREATOR BY : PRANKBOT
+#MOD BY ACIL
+__all__ = ['TServer', 'TNonblockingServer']
